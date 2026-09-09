739c0d23a391a6a8f197fd7e274870c3423028a4
progCoOdu <work.chill.coffee@gmail.com>
2026-09-09 16:24:15 +0300

    Update telegram.ts

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -1,5 +1,19 @@
 import { TelegramUser } from '../types'
 
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp: {
+        initDataUnsafe: {
+          user?: TelegramUser
+        }
+        expand: () => void
+        close: () => void
+      }
+    }
+  }
+}
+
 export const tg = window.Telegram?.WebApp
 
 export const getTelegramUser = (): TelegramUser | null => {
