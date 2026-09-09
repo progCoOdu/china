13ac4eacbe32f242d228ba2ff21fccef7aaf6e27
progCoOdu <work.chill.coffee@gmail.com>
2026-09-09 15:58:54 +0300

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+export interface Order {
+  id: string
+  tg_user_id: string
+  tg_username: string | null
+  tg_first_name: string | null
+  link: string
+  description: string
+  status: 'new' | 'in_progress' | 'done'
+  created_at: string
+}
+
+export interface TelegramUser {
+  id: number
+  first_name: string
+  last_name?: string
+  username?: string
+  language_code?: string
+}
