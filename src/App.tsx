3d6ff8ec0d642e5d7529693adf116f788effd57b
progCoOdu <work.chill.coffee@gmail.com>
2026-09-09 21:46:15 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
 import OrderForm from './pages/OrderForm'
 import MyOrders from './pages/MyOrders'
+import Finance from './pages/Finance'
 import Admin from './pages/Admin'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<OrderForm />} />
-        <Route path="/orders" element={<MyOrders />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
+      <div style={{ paddingBottom: '80px' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/order" element={<OrderForm />} />
+          <Route path="/orders" element={<MyOrders />} />
+          <Route path="/finance" element={<Finance />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </div>
+      <NavBar />
     </BrowserRouter>
   )
 }
